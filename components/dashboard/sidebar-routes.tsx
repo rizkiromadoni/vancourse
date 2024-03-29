041a4cd70991ce48d5050e7e936f894722f9306d
rizkiromadoni <coradex06@gmail.com>
2024-03-30 00:17:32 +0700

    feat: add sidebar

---
+++ components/dashboard/sidebar-routes.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Compass, Layout } from "lucide-react"
+import SidebarItem from "./sidebar-item"
+
+const guestRoutes = [
+    {
+        icon: Layout,
+        label: "Dashboard",
+        href: "/"
+    },
+    {
+        icon: Compass,
+        label: "Explore",
+        href: "/search"
+    }
+]
+
+const SidebarRoutes = () => {
+  const routes = guestRoutes
+
+  return (
+    <div className="flex flex-col w-full">
+        {routes.map((route) => (
+            <SidebarItem
+                key={route.href}
+                {...route}
+            />
+        ))}
+    </div>
+  )
+}
+
+export default SidebarRoutes
